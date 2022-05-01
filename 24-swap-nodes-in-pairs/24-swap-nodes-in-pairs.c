658678f898e3f4aca9f5dfd52404b87f22449f95
Hon Joo <64689788+Teddies1@users.noreply.github.com>
2022-05-01 21:34:16 +0800

    Time: 3 ms (43.28%), Space: 6 MB (38.80%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.c
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* swapPairs(struct ListNode* head){
+    
+    
+    if (head == NULL){
+        return NULL;
+    }
+    if (head->next == NULL){
+        return head;
+    }
+    struct ListNode *next = head->next;
+    struct ListBode *temp = head->next->next;
+    next->next = head;
+    head->next = swapPairs(temp);
+    
+    
+    
+    
+    
+
+   
+
+    
+    return next;
+}
