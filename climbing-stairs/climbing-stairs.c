9e6ec59c2eede67f596c038c661400324536dbcc
Hon Joo <64689788+Teddies1@users.noreply.github.com>
2022-05-12 00:29:37 +0800

    Time: 0 ms, Memory: 5.4 MB - LeetHub

--- climbing-stairs/climbing-stairs.c
+++ climbing-stairs/climbing-stairs.c
@@ -1,22 +1,22 @@
 
 
 int climbStairs(int n){
-    int array[46];
+    int array[45];
     
     // if you count n = 5,
     // notice that n[5] is n[4] + n[3]. basically n-1 + n-2
     
     //base cases up to n = 3
 
-    array[0] = 0;
-    array[1] = 1;
-    array[2] = 2;
+    array[0] = 1;
+    array[1] = 2;
     
     
-    for (int i = 3; i <= n; i++){
+    
+    for (int i = 2; i < n; i++){
         array[i] = array[i-1] + array[i-2];
     }
     
-    return array[n];
+    return array[n-1];
     
 }
