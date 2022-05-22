bc9426e27073503a589b5a2892ab843d752eb621
Hon Joo <64689788+Teddies1@users.noreply.github.com>
2022-05-22 12:43:00 +0800

    Time: 18 ms (52.81%), Space: 6.3 MB (5.37%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.c
@@ -0,0 +1,37 @@
+#include <math.h>
+
+
+bool isPalindrome(int x){
+    if (x != 0 && x % 10 == 0){
+        return false;
+    }
+    if (x < 0){
+        return false;
+    }
+    if (x == 0){
+        return true;
+    }
+    int count = 0;
+    int temp = x;
+    int res = 0;
+    int num = 0;
+    int i = 1;
+    
+    while(temp){
+        num = temp % 10;
+        count++;
+        temp /= 10;
+    }
+    temp = x;
+    while(count){
+        num = temp % 10;
+        res += num * pow(10, count-1);  
+        count--;
+        temp /= 10;
+    }
+    if (res == x){
+        return true;
+    }
+    
+    return false;
+}
