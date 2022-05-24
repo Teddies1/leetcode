5723c60a20f4793fcbd19f0cd0562af6e75d6730
Hon Joo <64689788+Teddies1@users.noreply.github.com>
2022-05-25 00:18:05 +0800

    Time: 60 ms (22.56%), Space: 7 MB (68.53%) - LeetHub

---
+++ 704-binary-search/704-binary-search.c
@@ -0,0 +1,23 @@
+int helper(int start, int end, int target, int* array){
+    if (start > end){
+        return -1;
+    }
+    int mid = (end + start)/2;
+    
+    if (array[mid] == target){
+        return mid;
+    }
+    if (target > array[mid]){
+        return helper(mid+1, end, target, array);
+    }
+    if (target < array[mid]){
+        return helper(start, mid-1, target, array);
+    }
+    return -1;
+    
+}
+
+int search(int* nums, int numsSize, int target){
+    int res = helper(0,numsSize-1,target,nums);
+    return res;
+}
