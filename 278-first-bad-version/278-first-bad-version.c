9b5d1aa44c878937db2a0fac8f137f78ed77345e
Hon Joo <64689788+Teddies1@users.noreply.github.com>
2022-05-28 13:06:37 +0800

    Time: 6 ms (10.06%), Space: 5.6 MB (21.24%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.c
+++ 278-first-bad-version/278-first-bad-version.c
@@ -5,11 +5,35 @@ int firstBadVersion(int n) {
     if (n == 1){
         return isBadVersion(1);
     }
+    /*
     for (int i = n; i >= 0; i--){
         if (isBadVersion(i-1) == false){
             return i;
             
         }
     }
-    return 0;
+    */
+    long upper = n;
+    long lower = 1;
+    long mid;
+    while(upper >= lower){
+        
+        mid = (upper + lower)/2;
+    
+        //if middle call is false, it means the 1st bad version is to the right
+        //if middle call is true, it means the 1st bad version is to the left
+        if(isBadVersion(mid) == false){
+            if (isBadVersion(mid+1) == true && isBadVersion(mid-1) == false){
+                return mid+1;
+            }
+            lower = mid+1;
+        }
+        else{
+            if (isBadVersion(mid+1) == true && isBadVersion(mid-1) == false){
+                return mid;
+            }
+            upper = mid-1;
+        }
+    }
+    return mid;
 }
