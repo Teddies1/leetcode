d9c6a22cb2da086608912de695c4c54c9eb55f06
Hon Joo <64689788+Teddies1@users.noreply.github.com>
2022-05-28 13:13:47 +0800

    Time: 5 ms (17.02%), Space: 5.4 MB (66.81%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.c
+++ 278-first-bad-version/278-first-bad-version.c
@@ -6,6 +6,7 @@ int firstBadVersion(int n) {
         return isBadVersion(1);
     }
     /*
+    // inefficient o(n) method
     for (int i = n; i >= 0; i--){
         if (isBadVersion(i-1) == false){
             return i;
@@ -13,22 +14,30 @@ int firstBadVersion(int n) {
         }
     }
     */
+    //idea is binary search, o(lg n)
     long upper = n;
     long lower = 1;
     long mid;
     while(upper >= lower){
-        
         mid = (upper + lower)/2;
-    
+        
         //if middle call is false, it means the 1st bad version is to the right
         //if middle call is true, it means the 1st bad version is to the left
         if(isBadVersion(mid) == false){
+            /*
+            if mid is false, left version is false, right version is true,
+            then the right version is the first bad version;
+            */
             if (isBadVersion(mid+1) == true && isBadVersion(mid-1) == false){
                 return mid+1;
             }
             lower = mid+1;
         }
         else{
+            /*
+            if mid is true, left version is false, right version is true,
+            then the mid version is the first bad one
+            */
             if (isBadVersion(mid+1) == true && isBadVersion(mid-1) == false){
                 return mid;
             }
