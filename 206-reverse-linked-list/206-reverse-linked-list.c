d4d4d5e79d5720d1067cf13e9b26b9c1d277d6f9
Hon Joo <64689788+Teddies1@users.noreply.github.com>
2022-05-28 12:34:34 +0800

    Time: 8 ms (20.01%), Space: 6.2 MB (85.37%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.c
+++ 206-reverse-linked-list/206-reverse-linked-list.c
@@ -5,7 +5,28 @@
  *     struct ListNode *next;
  * };
  */
-
+/*
+    first we set pre to NULL.
+    the idea is that we keep pointing cur->next backwards towards pre.
+    we keep doing this until cur == NULL, which is the terminating statement.
+    then, pre will be the new head node, and the LL is reverse since all the links-
+    point backwards, and eventually towards the original NULL pointer.
+    
+    NULL    1 -> 2 -> 3 -> 4 -> NULL
+    p       c    n
+    
+    NULL <- 1    2 -> 3 -> 4 -> NULL
+            p    c    n
+            
+    NULL <- 1 <- 2    3 -> 4 -> NULL
+                 p    c    n
+                 
+    NULL <- 1 <- 2 <- 3    4 -> NULL
+                      p    c    n
+                      
+    NULL <- 1 <- 3 <- 3 <- 4    NULL 
+                           p    c
+    */
 
 struct ListNode* reverseList(struct ListNode* head){
     struct ListNode *pre = NULL;
@@ -17,16 +38,19 @@ struct ListNode* reverseList(struct ListNode* head){
     }
     
     while(cur != NULL){   
+        //establish the next node
         next = cur->next;
+        
+        //break link to next and point it to pre
         cur->next = pre;
+        
+        //set cur node to pre
         pre = cur;
+        
+        //set next node to cur
         cur = next;
     }    
-    /*while(cur != NULL){   
-        next = reverseList(cur->next);
-        cur->next = pre;
-        pre = cur;
-        cur = next;
-    }*/
     return pre;
+    
+    
 }
