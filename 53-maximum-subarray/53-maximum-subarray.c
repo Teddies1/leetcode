0e449d6aa908c1de40820fa5138b2006d5248857
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-09 18:43:53 +0800

    Time: 123 ms (75.03%), Space: 12.4 MB (49.25%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.c
+++ 53-maximum-subarray/53-maximum-subarray.c
@@ -5,21 +5,7 @@ int max(int a, int b){
 int maxSubArray(int* nums, int numsSize){
     int newsum = 0x80000000;
     int currentsum = 0;
-    if (numsSize == 1){
-        return nums[0];
-    }
-    /*
-    -2 -2
-    -1  1
-    -4  1
-     0  4
-    -1  4
-     1  4
-     
-    */    
-        
-        
-        
+      
     for (int i = 0; i < numsSize; i++){
         currentsum += nums[i];
         newsum = max(newsum, currentsum);
