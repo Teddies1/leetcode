f421a1c03f03892cdffc249f304155ce2bbad50e
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-09 22:35:13 +0800

    Time: 6 ms (12.63%), Space: 6 MB (37.90%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.c
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.c
@@ -8,16 +8,33 @@
 
 
 struct ListNode* middleNode(struct ListNode* head){
+    
+    // okay-ish solution, but it is o(2n)
+    /*
     int count = 0;
     struct ListNode *temp = head;
-    
+   
     while(temp != NULL){
         count++;
         temp = temp->next;
     }
-    for (int i = 0; i < (count/2) ; i++){
+    for (int i = 0; i < (count/2); i++){
         head = head->next;
     }
     return head;
+    */
+    
+    //o(n)
+    struct ListNode *fast = head;
+    struct ListNode *slow = head;
+    
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+    
+    
+    
     
 }
