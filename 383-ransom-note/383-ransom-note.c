90d6ddd62fb0387aa1057ff3becbb602978a1d52
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-11 17:31:15 +0800

    Time: 11 ms (43.36%), Space: 6.6 MB (18.88%) - LeetHub

--- 383-ransom-note/383-ransom-note.c
+++ 383-ransom-note/383-ransom-note.c
@@ -1,6 +1,18 @@
 
 
 bool canConstruct(char * ransomNote, char * magazine){
+    /* 
+    idea is to maintain an 26 member array, initialised to 0,
+    storing the counter of alphabetical characters in the magazine string.
+    then we traverse the magazine string, incrementing the array entry
+    corresponding to the letter (a -> array[0], etc.)
+    
+    then we traverse the ransomNote string. for each letter,
+    first check if the array entry is empty. if yes, the note 
+    cannot be complete, and return false.
+    if it's not empty, then decrement it by 1. 
+    */
+    
     int mag[26] = {0};
     int letter, ransom;
     int i = 0, j = 0;
