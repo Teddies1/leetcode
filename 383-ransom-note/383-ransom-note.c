3786fe1c2355befe8c0cbbdc3df1e8ce6974566e
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-11 17:16:10 +0800

    Time: 246 ms (9.13%), Space: 6.6 MB (18.88%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.c
@@ -0,0 +1,30 @@
+
+
+bool canConstruct(char * ransomNote, char * magazine){
+    int mag[26] = {0};
+    int letter, ransom;
+    int i = 0, j = 0;
+    while (magazine[i] != '\0'){
+        letter = magazine[i] - 97;
+        mag[letter]++;
+        printf("%d", mag[letter]);
+        i++;
+    }
+    while (ransomNote[j] != '\0'){
+        ransom = ransomNote[j] - 97;
+        if (mag[ransom] == 0){
+            return false;
+        }
+        if (mag[ransom] > 0){
+            mag[ransom]--;
+        }
+        printf("%d", mag[ransom]);
+
+        
+        j++;
+    }
+    
+    
+    
+    return true;
+}
