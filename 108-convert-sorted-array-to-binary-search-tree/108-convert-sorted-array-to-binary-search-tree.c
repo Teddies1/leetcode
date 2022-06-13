4f416d85f5c383f0a703ccd00939fabc37b26462
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-13 20:48:54 +0800

    Time: 11 ms (62.88%), Space: 16.4 MB (23.74%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.c
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.c
@@ -7,21 +7,20 @@
  * };
  */
 struct TreeNode* binsearch(int start, int end, int* nums){
-    struct TreeNode *root = malloc(sizeof(struct TreeNode));
     int mid = (start + end)/2;
+    
     if (start > end){
         return NULL;
+    
     }
+    struct TreeNode *root = malloc(sizeof(struct TreeNode));
     root->val = nums[mid];
     root->left = binsearch(start, mid-1, nums);
     root->right = binsearch(mid+1, end, nums);
-    
-    
+     
     return root;
-    
 }
 
 struct TreeNode* sortedArrayToBST(int* nums, int numsSize){
-    
     return binsearch(0, numsSize-1, nums);
 }
