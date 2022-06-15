e9862525fa6f8c3e5c4e1f933b883be5510bc2ea
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-15 18:58:36 +0800

    Time: 35 ms (15.36%), Space: 5.6 MB (60.56%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.c
@@ -0,0 +1,18 @@
+
+
+int mySqrt(int x){
+    long long square, prev, i;
+    if (x < 4 && x != 0){
+        return 1;
+    }
+    for (i = 1; i <= (x/2)+1; i++){
+        square = i * i;
+        if (square > x){
+            return i-1;
+        }
+        if (square == x){
+            return i;
+        }
+    }
+    return 0;
+}
