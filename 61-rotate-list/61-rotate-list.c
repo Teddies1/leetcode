d1ae9269eb5a61e4c309f6a0f55f46bcd755551f
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-15 18:11:21 +0800

    Time: 3 ms (82.40%), Space: 6.2 MB (56.12%) - LeetHub

--- 61-rotate-list/61-rotate-list.c
+++ 61-rotate-list/61-rotate-list.c
@@ -14,16 +14,26 @@ struct ListNode* rotateRight(struct ListNode* head, int k){
         return NULL;
     }
     int count = 1;
+    /* we want to create a circular linked list, and maintain a counter */
     while (head->next != NULL){
         head = head->next;
         count++;
     }
     head->next = temp;
+    /*  
+        notice that the head node to be returned is simply 
+        (count - (k % count)) + 1 times of head = head->next;
+    */
     int diff = k % count;
     
     for (int i = 0; i < count - diff; i++){
         head = head->next;
     }
+    /* 
+        increment the head by count-diff times, and set the head to be returned as head->next
+        then, break the circular linked list
+    */
+    
     rotatehead = head->next;
     head->next = NULL;
     return rotatehead;
