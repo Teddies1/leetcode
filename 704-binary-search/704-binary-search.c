0bdfe9a1b0d03b38d3c43bab39f6f918583c996a
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-19 12:25:43 +0800

    Time: 96 ms (5.20%), Space: 7 MB (67.75%) - LeetHub

--- 704-binary-search/704-binary-search.c
+++ 704-binary-search/704-binary-search.c
@@ -1,3 +1,4 @@
+/*
 int helper(int start, int end, int target, int* array){
     if (start > end){
         return -1;
@@ -11,10 +12,30 @@ int helper(int start, int end, int target, int* array){
     if (target > array[mid]){
         return helper(mid+1, end, target, array);
     }
-    return helper(start, mid-1, target, array);;
+    return helper(start, mid-1, target, array);
     
 }
-
+*/
 int search(int* nums, int numsSize, int target){
-    return helper(0,numsSize-1,target,nums);
+    //return helper(0,numsSize-1,target,nums);
+    
+    int start = 0, end = numsSize-1;
+    int mid;
+    
+    while(start <= end){
+        mid = (start + end)/2;
+        if (nums[mid] == target){
+            return mid;
+        }
+        if (nums[mid] < target){
+            start = mid + 1;
+        }
+        if (nums[mid] > target){
+            end = mid - 1;
+        }
+    }
+    
+    return -1;
+    
+    
 }
