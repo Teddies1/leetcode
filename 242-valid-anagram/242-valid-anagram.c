b4da5deb0c100fe7c8399d6d42668af3d3077828
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-19 22:00:28 +0800

    Time: 6 ms (53.82%), Space: 5.8 MB (96.03%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.c
+++ 242-valid-anagram/242-valid-anagram.c
@@ -4,7 +4,7 @@ bool isAnagram(char * s, char * t){
     int array1[26] = {0};
     int array2[26] = {0};
     int letter1, letter2, i = 0, j = 0, m = 0, n = 0;
-    while (s[i] != '\0' && t[j] != '\0'){
+    while (s[i] != '\0'){
         letter1 = s[i] - 'a';
         array1[letter1]++;
         i++;
@@ -14,12 +14,10 @@ bool isAnagram(char * s, char * t){
         array2[letter2]++;
         j++;
     }
-    while (m < 26 && n < 26){
-        if (array1[m] != array2[n]){
+    for (int k = 0; k < 26; k++){
+        if (array1[k] != array2[k]){
             return false;
         }
-        m++;
-        n++;
     }
     return true;
 }
