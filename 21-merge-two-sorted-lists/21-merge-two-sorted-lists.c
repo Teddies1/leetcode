7bbc9fc0f60feec0cd80415f3781966edc618d8e
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-06-25 17:36:18 +0800

    Time: 10 ms (15.64%), Space: 6.1 MB (69.62%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.c
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.c
@@ -8,6 +8,7 @@
 
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    /*
     if (!list1){
         return list2;
     }
@@ -22,5 +23,33 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
         list1->next = mergeTwoLists(list1->next, list2);
         return list1;
     }
-    
+    */
+    struct ListNode dummy;
+    struct ListNode *temp = &dummy;
+    if (list1 == NULL && list2 == NULL){
+        return NULL;
+    }
+    while(list1 != NULL && list2 != NULL){
+        if (list1->val > list2->val){
+            temp->next = list2;
+            temp = temp->next;
+            list2 = list2->next;
+        }
+        else{
+            temp->next = list1;
+            temp = temp->next;
+            list1 = list1->next;
+        }
+    }
+    while(list1 != NULL){
+        temp->next = list1;
+        temp = temp->next;
+        list1 = list1->next;
+    }
+    while(list2 != NULL){
+        temp->next = list2;
+        temp = temp->next;
+        list2 = list2->next;
+    }
+    return dummy.next;
 }
