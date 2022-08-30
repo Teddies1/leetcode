e57d04f6d8de324ba2c80f2903241a019c69a6fc
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-08-30 16:42:41 +0800

    Time: 0 ms (100.00%), Space: 42.7 MB (28.97%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
+        /*
         int[] colors = new int[3];
         int j = 0;
         
@@ -10,6 +11,27 @@ class Solution {
             while(colors[i] > 0){
                 nums[j++] = i;
                 colors[i]--;
+            }
+        }
+        */
+        int low = 0, high = nums.length-1, iter = 0;
+        int temp;
+        while (iter <= high){
+            if(nums[iter] == 0){
+                temp = nums[iter];
+                nums[iter] = nums[low];
+                nums[low] = temp;
+                low++; 
+                iter++;
+            }
+            else if(nums[iter] == 1){
+                iter++;
+            }
+            else{
+                temp = nums[iter];
+                nums[iter] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
     }
