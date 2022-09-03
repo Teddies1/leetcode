d6048c3d9d29c80b9d473152e1bfa6575fe9911f
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-09-03 18:00:05 +0800

    Time: 14 ms (17.41%), Space: 43.9 MB (71.02%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -3,20 +3,15 @@ class Solution {
         Stack<Integer> s = new Stack<Integer>();
         
         for (String token: tokens){
-            
             if (token.equals("+") || token.equals("/") || token.equals("-") || token.equals("*")){
                 Integer one = Integer.valueOf(s.pop());
                 Integer two = Integer.valueOf(s.pop());
-                System.out.printf("%d %d\n", one, two);
                 switch(token){
                     case "+": 
                         s.push(one + two);
                         break;
-                    case "/": 
-                        
-                     
-                            s.push(two / one);
-                        
+                    case "/":      
+                        s.push(two / one);
                         break;
                     case "*": 
                         s.push(one * two);
