ee6b1992b912061905e8c412b8eca0867dcfcbc7
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-09-03 18:03:52 +0800

    Time: 6 ms (91.45%), Space: 44.1 MB (51.26%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -2,31 +2,27 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> s = new Stack<Integer>();
         Integer one, two;
-        for (String token: tokens){
-            
-                
-                switch(token){
-                    case "+": 
-                        s.push(s.pop() + s.pop());
-                        break;
-                    case "/":      
-                         one = Integer.valueOf(s.pop());
-                         two = Integer.valueOf(s.pop());
-                        s.push(two / one);
-                        break;
-                    case "*": 
-                        s.push(s.pop() * s.pop());
-                        break;
-                    case "-": 
-                         one = Integer.valueOf(s.pop());
-                         two = Integer.valueOf(s.pop());
-                        s.push(two - one);
-                        break;
-                    default:
-                        s.push(Integer.valueOf(token));
+        for (String token: tokens){  
+            switch(token){
+                case "+": 
+                    s.push(s.pop() + s.pop());
+                    break;
+                case "/":      
+                    one = Integer.valueOf(s.pop());
+                    two = Integer.valueOf(s.pop());
+                    s.push(two / one);
+                    break;
+                case "*": 
+                    s.push(s.pop() * s.pop());
+                    break;
+                case "-": 
+                    one = Integer.valueOf(s.pop());
+                    two = Integer.valueOf(s.pop());
+                    s.push(two - one);
+                    break;
+                default:
+                    s.push(Integer.valueOf(token));
                 }
-                
-        
         }
         return s.pop();
     }
