b7d2dbe2df8651e1bb8a65fc85f922f60951f522
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-09-18 20:45:48 +0800

    Time: 2 ms (93.57%), Space: 40.1 MB (99.27%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        s.toLowerCase();
+        int count = 0;
+        int flag = 0;
+        int[] letter = new int[58];
+        
+        for (int i = 0; i < s.length(); i++){
+            int word = s.charAt(i) - 65;
+            letter[word]++;
+        }
+        for (int i = 0; i < letter.length; i++){
+            if (letter[i] % 2 == 0){
+                count += letter[i];
+            }
+            else{
+                flag = 1;
+                count += letter[i] - 1;
+            }
+        }
+        if (flag == 1){
+            count++;
+        }
+        return count;
+        
+    }
+}
