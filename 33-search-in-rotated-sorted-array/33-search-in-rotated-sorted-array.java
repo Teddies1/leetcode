ec9d18105a7c0e8b9168fb6bc9dd2f46dfb47497
honjoo <64689788+Teddies1@users.noreply.github.com>
2022-09-18 14:11:32 +0800

    Time: 1 ms (77.24%), Space: 42.3 MB (75.49%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        /*  the idea is that we have to find an point of decrease. 
+            then, the array before this point are the 'right' subarray
+            and the array after is the 'left' array.
+            
+            then the original 'midpoint' is the first element of the array.
+            after that perform binary search. if there is no point of decrease
+            then use binary search normally    
+        */
+        
+        return helper(nums, 0, nums.length-1, target);
+    }
+    public int helper(int[] array, int start, int end, int target){
+        int mid;
+        while (start <= end){
+            mid = start + (end - start) / 2;
+            if (target == array[mid]){
+                return mid;
+            }
+            else if (array[start] <= array[mid]){
+                if (target < array[start] || target > array[mid]){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+            else{
+                if (target > array[end] || target < array[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
