1ae98fec3a2f87ea61e9940a8cef8cc8322e88bb
honjoo <64689788+Teddies1@users.noreply.github.com>
2023-03-06 21:49:45 +0800

    Time: 2 ms (45.21%), Space: 51 MB (27.80%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -13,11 +13,9 @@ class Solution {
         suffix[size-1] = 1;
         for (int i = 1; i < size; i++){
             prefix[i] = prefix[i-1] * nums[i-1];
-            System.out.println(prefix[i]);
         }
         for (int i = size-2; i >= 0; i--){
             suffix[i] = suffix[i+1] * nums[i+1];
-            System.out.println(suffix[i]);
         }
         for (int i = 0; i < size; i++){
             res[i] = suffix[i] * prefix[i];
