ca737e38b0948eee056237bfb4fc1c844b4462c3
honjoo <64689788+Teddies1@users.noreply.github.com>
2023-09-23 14:03:24 +0800

    Time: 94 ms (81.59%), Space: 21 MB (63.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,16 +3,20 @@ class Solution:
         heap = []
         ans = []
         hashmap = {}
+        
         for num in nums:
             if (num in hashmap):
                 hashmap[num] += 1
             else:
                 hashmap[num] = 1
+                
         for key, value in hashmap.items():
             heapq.heappush(heap, (-value, key))
-        print(heap)
+            
+        
         for i in range(k):            
             ans.append(heapq.heappop(heap)[1])
+            
         return ans
             
         
