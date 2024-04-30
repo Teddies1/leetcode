ce77d62869293158457fd7ce6a39a80b0bf9d18b
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-04-30 22:32:54 +0800

    Time: 55 ms (28.07%) | Memory: 17 MB (33.85%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        smap = {}
+        tmap = {}
+        
+        for i in range(len(s)):
+            if s[i] not in smap:
+                smap[s[i]] = 1
+            else:
+                smap[s[i]] += 1
+        for i in range(len(t)):
+            if t[i] not in tmap:
+                tmap[t[i]] = 1
+            else:
+                tmap[t[i]] += 1
+        
+        print(smap, tmap)
+        if smap == tmap: 
+            return True
+        return False
+    
