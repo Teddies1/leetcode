0579808d8e58bb10c71f584ea8bd4cd6fe86f03e
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-06-16 19:50:19 +0800

    Time: 47 ms (5.06%) | Memory: 16.6 MB (52.31%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        map = {}
+        ans = 0
+        flag = 0
+        for c in s:
+            if c not in map:
+                map[c] = 1
+            else:
+                map[c] += 1
+                
+        for k, v in map.items():
+            print(k , v)
+            if v % 2 == 0:
+                ans += v
+            elif v > 1:
+                ans += v-1
+                flag = 1
+            else: 
+                flag = 1
+        
+        if flag == 1:
+            ans += 1
+            
+        return ans
+            
