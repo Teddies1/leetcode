4ed83e521d3ce1abb84539a329c513c365cc4579
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-06-21 01:05:22 +0800

    Time: 37 ms (62.54%) | Memory: 16.5 MB (75.13%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,7 +3,7 @@ class Solution:
         curmax = strs[0]
         for str in strs:
             i = 0
-            while i < len(str) and i < len(curmax) and str[i] == curmax[i]:
+            while i < min(len(str), len(curmax)) and str[i] == curmax[i]:
                 i += 1
             if i == 0:
                 return ""
