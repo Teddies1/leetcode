f4d96b6fa61c6cccf447afaf1c57952e68620539
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-06-22 20:06:39 +0800

    Time: 65 ms (15.76%) | Memory: 16.7 MB (10.88%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,13 +4,12 @@ class Solution:
             return False
         if x < 10:
             return True
+        
         stack = []
-        leng = 0
         while x:
-            
             stack.append(x % 10)
             x //= 10
-        
+            
         left, right = 0, len(stack)-1
         while left < right:
             if stack[left] != stack[right]:
