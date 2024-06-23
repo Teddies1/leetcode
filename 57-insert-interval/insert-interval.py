884c0da15632f2c046ca33ee5d0d1a8c32d37207
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-06-23 23:29:49 +0800

    Time: 71 ms (78.87%) | Memory: 19.9 MB (22.70%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -5,12 +5,10 @@ class Solution:
             for interval in intervals:
                 if newInterval[0] > interval[1]:
                     ans.append(interval)
-                    
             for interval in intervals:
                 if interval[0] < newInterval[0]:
                     if interval[1] >= newInterval[0]:
                         newInterval[0] = interval[0]
-                        
             for interval in intervals:
                 if newInterval[1] < interval[1]:
                     if newInterval[1] >= interval[0]:
