c7e594b7fb9c587b931fa51421160a20d81a5c39
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-06-24 00:20:39 +0800

    Time: 525 ms (15.24%) | Memory: 29.5 MB (86.30%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -4,11 +4,8 @@ class Solution:
         
         for num in nums:
             heapq.heappush(heap, (num * -1))
-            
-        print(heap)
-        
+                
         for i in range(k-1):
             heapq.heappop(heap)
-        
         
         return heap[0] * -1            
