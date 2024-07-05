90497dd16563f4d7e049033302f9d2efbc22be40
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-07-05 16:40:25 +0800

    Time: 59 ms (87.90%) | Memory: 16.9 MB (81.64%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
@@ -1,16 +1,14 @@
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        ans = []
         if len(nums) == 1:
             nums[0].sort()
             return nums[0]
         
-        set1 = set(nums[0])
+        ans = set(nums[0])
         for i in range(1, len(nums)):
-            set1 = set1 & set(nums[i])
+            ans = ans & set(nums[i])
         
-        
-        ans = list(set1)
+        ans = list(ans)
         ans.sort()
         
         return ans
