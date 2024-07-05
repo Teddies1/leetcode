164071319a2edf4351806bf0fb8476ef93dbab4d
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-07-05 15:32:42 +0800

    Time: 72 ms (5.00%) | Memory: 16.7 MB (25.44%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -8,24 +8,19 @@ class Solution:
                 map1[num] += 1
             else: 
                 map1[num] = 1
-        for num in nums2:
-            if num in map2:
-                map2[num] += 1
-            else: 
-                map2[num] = 1
+        # for num in nums2:
+        #     if num in map2:
+        #         map2[num] += 1
+        #     else: 
+        #         map2[num] = 1
                 
         ans = []
         
-        for k1, v1 in map1.items():
-            for k2, v2 in map2.items():
-                if k1 == k2:
-                    ans += min(v2, v1) * [k1]
         
-        
-        
-        
-        
-        
-        
-        
+        for k, v in map1.items():
+            for num in nums2:
+                if num == k and v > 0:
+                    ans.append(num)
+                    v -= 1
+                    
         return ans
