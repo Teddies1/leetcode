9df87d196b9d22a891571a7c4aa5bd78903e0e1a
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-07-05 15:35:21 +0800

    Time: 83 ms (5.00%) | Memory: 16.6 MB (93.36%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,21 +1,14 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         map1 = {}
-        map2 = {}
         
         for num in nums1:
             if num in map1:
                 map1[num] += 1
             else: 
                 map1[num] = 1
-        # for num in nums2:
-        #     if num in map2:
-        #         map2[num] += 1
-        #     else: 
-        #         map2[num] = 1
-                
+
         ans = []
-        
         
         for k, v in map1.items():
             for num in nums2:
