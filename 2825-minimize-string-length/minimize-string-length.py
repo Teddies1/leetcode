c0f21988fc9af28525fc835373cb358952e0903e
honjoo <64689788+Teddies1@users.noreply.github.com>
2024-07-05 17:03:00 +0800

    Time: 50 ms (51.21%) | Memory: 16.5 MB (29.05%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimizedStringLength(self, s: str) -> int:
+        stringset = set(s)
+        
+        
+        
+        
+        return len(stringset) if len(stringset) > 0 else ""
