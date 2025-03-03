2ffbe18d1839e4adb92c558feb146d7f1df76d91
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-03-03 22:34:49 +0800

    Time: 36 ms (46.97%) | Memory: 34.6 MB (57.71%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,6 +1,5 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        hashmap = {}
         less, equal, greater = [], [], []
         ans = []
         for num in nums:
@@ -11,11 +10,11 @@ class Solution:
             else:
                 greater.append(num)
 
-        print(less, greater, equal)
         for l in less:
             ans.append(l)
         for e in equal:
             ans.append(e)
         for g in greater:
             ans.append(g)
+
         return ans
