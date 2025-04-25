89b690741087b6b67f605c3f505c0bbeee0fa307
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-04-25 18:46:52 +0800

    Time: 4 ms (69.61%) | Memory: 21.3 MB (62.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,26 +1,10 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {}
-        for num in nums:
-            if num in hashmap:
-                hashmap[num] += 1
-            else:
-                hashmap[num] = 1
+        hashmap = Counter(nums)
         
-        # hashmap = dict(sorted(hashmap.items(), key=lambda x: x[1], reverse=True))
-        # ans = []
-
-        # for key, val in hashmap.items():
-        #     if k == 0:
-        #         return ans
-        #     ans.append(key)
-        #     k -= 1
-
-        # return ans
-
         heap = [(-value, key) for key, value in hashmap.items()]
         heapq.heapify(heap)
-        
+
         ans = []
         for _ in range(k):
             value, key = heapq.heappop(heap)
