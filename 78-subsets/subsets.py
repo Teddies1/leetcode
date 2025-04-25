36544115d8549311132996d39ec2f494f6b734af
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-04-25 23:33:16 +0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (40.08%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        '''
+        start from empty list and index
+        at each index, can make a choice each time to add or not add
+        when we add number, continue recursing by iterating to next index
+        terminate when index reaches length of nums
+        remove added number to backtrack to other paths
+        '''
+
+        ans = []
+        self.recurse(nums, 0, ans, [])
+        return ans
+
+    def recurse(self, nums, index, ans, sub_array):
+        if len(nums) <= index:
+            print(sub_array)
+            ans.append(sub_array[:])
+            return
+
+        sub_array.append(nums[index])
+        self.recurse(nums, index+1, ans, sub_array)
+        sub_array.pop()
+        self.recurse(nums, index+1, ans, sub_array)
