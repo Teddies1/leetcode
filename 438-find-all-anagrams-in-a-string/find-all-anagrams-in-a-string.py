0059544952f2695259ea12d1886fbb4946c94356
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-04-25 23:08:52 +0800

    Time: 211 ms (34.73%) | Memory: 18.4 MB (36.29%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        n = len(s)
+        m = len(p)
+        p_hashmap = Counter(p)
+        s_hashmap = Counter(s[:m])
+
+        if p_hashmap == s_hashmap:
+            ans = [0]
+        else:
+            ans = []
+        left = 0
+        for right in range(m, n):
+            s_hashmap[s[left]] -= 1
+            if s_hashmap[s[left]] == 0:
+                s_hashmap.pop(s[left])
+            left += 1
+            s_hashmap[s[right]] = s_hashmap.get(s[right], 0) + 1
+            if p_hashmap == s_hashmap:
+                ans.append(right - m + 1)
+        
+        return ans
