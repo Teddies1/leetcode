1e503bb86f66863bcedeeae6e032f441870088a7
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-04-26 23:00:06 +0800

    Time: 19 ms (61.04%) | Memory: 17.6 MB (97.09%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -17,6 +17,7 @@ class Solution:
     def recurse(self, candidates, index, ans, target, path, path_val):
         if path_val == target and not path in ans:
             ans.append(path[:])
+            return
         if path_val > target:
             return
 
@@ -26,3 +27,31 @@ class Solution:
             path.append(candidates[i])
             self.recurse(candidates, i+1, ans, target, path, path_val + candidates[i])
             path.pop()
+
+    '''
+    [1,1,2,5,6,7,10]
+    dry run:
+    recurse:
+        index = 0, target = 8, path = [], pathval = 0
+    for i in range(0, len(candidates))
+    path = [1]
+    recurse:
+        index = 1, path = [1], pathval = 1
+    for i in range(1, len(candidates))
+        candidates[1] == candidates[0]
+        continue
+        path = [1, 2]
+    recurse:
+        index = 3, path = [1, 2], pathval = 3
+    for i in range(3, len(candidates))
+        path = [1, 2, 5]
+    recurse:
+        index = 4, path = [1, 2, 5], pathval = 8
+    pathval == target
+    ans = [[1,2,5]]
+    return
+
+
+
+    
+    '''
