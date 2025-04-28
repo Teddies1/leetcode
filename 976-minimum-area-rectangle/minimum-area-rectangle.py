add58aecc6d27a812d9b3d12e5b2bda0c6edb6c8
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-04-28 22:43:39 +0800

    Time: 1668 ms (12.16%) | Memory: 18.2 MB (27.21%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.py
+++ 976-minimum-area-rectangle/minimum-area-rectangle.py
@@ -4,6 +4,7 @@ class Solution:
         for point in points:
             x, y = point
             point_set.add((x, y))
+            
         ans = float("inf")
         for x1, y1 in point_set:
             for x2, y2 in point_set:
@@ -11,4 +12,5 @@ class Solution:
                     if (x1, y2) in point_set and (x2, y1) in point_set:
                         area = abs(x1 - x2) * abs(y1 - y2)
                         ans = min(ans, area)
+
         return ans if ans != float("inf") else 0
