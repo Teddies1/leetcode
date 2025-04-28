ce2e6018aa18c813d652acbd7c9f08db0eb78c91
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-04-28 23:09:24 +0800

    Time: 25 ms (49.93%) | Memory: 18.5 MB (42.25%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        max_area = 0
+        n = len(grid)
+        m = len(grid[0])
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    area = self.recurse(grid, i, j)
+                    print(area)
+                    max_area = max(area, max_area)
+
+        return max_area
+
+    def recurse(self, grid, row, col):
+        n = len(grid)
+        m = len(grid[0])
+
+        if row < 0 or row >= n or col < 0 or col >= m:
+            return 0
+        if grid[row][col] == 0:
+            return 0
+        
+        grid[row][col] = 0
+
+        neighbours = [
+            (0, 1),
+            (0, -1),
+            (1, 0),
+            (-1, 0),
+        ]
+        cumulative_area = 1
+
+        for dx, dy in neighbours:
+            cumulative_area += self.recurse(grid, row + dx, col + dy)
+        
+        return cumulative_area
+    
+    '''
+    idea is to traverse entire grid
+    if we find a land, set current area to 1
+    do recursive dfs to get the area of the whole island
+    check if area is greater than current max
+    '''
