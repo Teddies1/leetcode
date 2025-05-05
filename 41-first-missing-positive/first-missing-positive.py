b60016a8f770bb6654065e819143372c4de42cb5
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-05-05 23:39:54 +0800

    Time: 56 ms (29.24%) | Memory: 28.9 MB (89.69%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,16 +1,32 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        visited = set()
+        # visited = set()
 
-        for num in nums:
-            if num >= 0:
-                visited.add(num)
+        # for num in nums:
+        #     if num >= 0:
+        #         visited.add(num)
 
-        if not visited:
-            return 1
+        # if not visited:
+        #     return 1
 
-        for i in range(1, max(visited) + 1):
-            if i not in visited:
-                return i
+        # for i in range(1, max(visited) + 1):
+        #     if i not in visited:
+        #         return i
         
-        return max(visited) + 1
+        # return max(visited) + 1
+        n = len(nums)
+        i = 0
+        while i < n:
+            if 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                swap(nums, i, nums[i] - 1)
+            else:
+                i += 1
+        
+        for i in range(n):
+            if nums[i] != (i + 1):
+                return i + 1
+        
+        return n + 1
+        
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
