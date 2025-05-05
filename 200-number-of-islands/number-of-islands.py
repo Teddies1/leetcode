29947ce333dccd9236423ea0b787bb221fc5579b
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-05-05 22:51:47 +0800

    Time: 252 ms (51.52%) | Memory: 20.2 MB (68.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -6,19 +6,18 @@ class Solution:
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     ans += 1
-                self.floodfill(grid, i, j, "0")
+                self.floodfill(grid, i, j)
         
         return ans
     
-    def floodfill(self, arr, row, col, char):
-        if row < 0 or col < 0 or row >= len(arr) or col >= len(arr[0]) or arr[row][col] == char:
+    def floodfill(self, arr, row, col):
+        if row < 0 or col < 0 or row >= len(arr) or col >= len(arr[0]) or arr[row][col] == "0":
             return 
         
-        if arr[row][col] != char:
-            arr[row][col] = char
+        arr[row][col] = "0"
         
-        self.floodfill(arr, row+1, col, char)
-        self.floodfill(arr, row, col-1, char)
-        self.floodfill(arr, row-1, col, char)
-        self.floodfill(arr, row, col+1, char)
+        self.floodfill(arr, row+1, col)
+        self.floodfill(arr, row, col-1)
+        self.floodfill(arr, row-1, col)
+        self.floodfill(arr, row, col+1)
         
