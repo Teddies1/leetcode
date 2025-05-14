58de49ec59e9ef34e715b23a76e7ccd0357eeca3
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-05-15 00:57:24 +0800

    Time: 83 ms (15.54%) | Memory: 21.2 MB (78.15%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,47 +1,18 @@
-class PFNode:
-    def __init__(self):
-        self.hashmap = {}
-        self.marker = False
-
-class PrefixTree:
-    def __init__(self):
-        self.tree = PFNode()
-        self.ans = []
-
-    def insert(self, val):
-        curr = self.tree
-        val = str(val)
-        for c in val:
-            if c not in curr.hashmap:
-                curr.hashmap[c] = PFNode()
-            curr = curr.hashmap[c]
-        curr.marker = True
-    
-    def retrieve(self, node, state):
-        if node.marker:
-            self.ans.append(int(state))
-        
-        for k, v in node.hashmap.items():
-            self.retrieve(v, state + k)
-        return
-
-
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        prefix_tree = PrefixTree()
+        ans = []
 
-        for i in range(1, n+1):
-            prefix_tree.insert(i)
+        for i in range(1, 10):
+            self.dfs(i, n, ans)
+        return ans
+
+    def dfs(self, number, target, ans):
+        if number > target:
+            return
         
-        prefix_tree.retrieve(prefix_tree.tree, "")
+        ans.append(number)
 
-        return prefix_tree.ans
-
-'''
-idea is to create a 'trie' for numbers
-create a node called Node
-each node contains a hashmap, which contains digits from 0 to 9
-each node also contains a marker, which is true when there is an associated value stored in that node digit
-
-each key in the hashmap points to another node, which contains the next digit of a number
-'''
+        for i in range(10):
+            new_number = (number * 10) + i
+            self.dfs(new_number, target, ans)
+        
