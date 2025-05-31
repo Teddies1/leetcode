36b94abcad4438cc348bf3c2be3328da02051e75
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-06-01 01:28:36 +0800

    Time: 62 ms (71.53%) | Memory: 22.9 MB (40.87%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,13 +1,18 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        heap, ans = [], []
-        
-        for point in points:
-            distance = pow(point[0], 2) + pow(point[1], 2)
-            heapq.heappush(heap, [distance, point])
-        
-        for i in range(k):       
-            item = heapq.heappop(heap)
-            ans.append(item[1])
-            
+        '''
+        idea is to build and maintain a heap that is heapified based on the euclidean distance (x^2 + y^2)
+        once heap is constructed, return the k closest points, which is equal to taking the root, deleting root and heapifying k times
+        '''
+
+        heap = []
+        ans = []
+        for x, y in points:
+            euclidean = x**2 + y**2
+            heapq.heappush(heap, (euclidean, x, y))
+
+        for _ in range(k):
+            _, x, y = heapq.heappop(heap)
+            ans.append([x, y])
+
         return ans
