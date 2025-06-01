b2ef11207d0986ea29acfb57e799f012be567f35
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-06-01 18:25:13 +0800

    Time: 23 ms (32.45%) | Memory: 17.9 MB (53.02%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,16 +1,41 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left, ans = 0, 0
-        dict = {}
-        
-        for right in range(len(s)):
-            if s[right] not in dict:
-                dict[s[right]] = 1
-            else:
-                dict[s[right]] += 1
-                while dict[s[right]] > 1:
-                    dict[s[left]] -= 1
+        '''
+        need to find some way to keep track of the longest substring with unique characters
+        possibly use hashmap as a frequency counter: ensure that all frequencies == 1
+
+        probably maintain some kind of sliding window
+        sliding window will always keep track of the substring with unique characters
+
+        left and right ptr
+
+        condition: all char frequencies in hashmap == 1
+
+        advance right pointer and add character to frequency map
+        check if frequency map meets our condition
+        if condition violated, means that char pointed by left pointer violates condition
+        advance left pointer until condition is met
+
+        check for current substring length and update max if needed
+        tc: o(n) -> one pass using right pointer, left pointer will trail behind right pointer
+        sc: o(n) -> hashmap to store frequencies, worst case all characters inside hashmap at one time
+        '''
+
+        max_length = 0
+        hashmap = defaultdict(int)
+        left = 0
+        n = len(s)
+        for i in range(n):
+            char = s[i]
+            # add to hashmap
+            hashmap[char] += 1
+            # make sure condition is met
+            if hashmap[char] > 1:
+                while hashmap[char] > 1:
+                    hashmap[s[left]] -= 1
                     left += 1
-            ans = max(ans, right - left + 1)
-        
-        return ans
+            # check substring length
+            substring_len = i - left + 1
+            max_length = max(max_length, substring_len)
+
+        return max_length
