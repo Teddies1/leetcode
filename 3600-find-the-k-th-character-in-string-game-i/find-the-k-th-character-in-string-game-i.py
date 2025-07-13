e76232e1b636e69d5ee1364a4a613049984b7ae3
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-07-13 16:02:23 +0800

    Time: 16 ms (15.38%) | Memory: 17.8 MB (56.86%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        word = "a"
+
+        while len(word) < k:
+            intermediate_word = self.operation(word)
+            word += intermediate_word
+            print(word)
+        return word[k-1]
+
+    def operation(self, word: str) -> str:
+        new_word = ""
+        for char in word:
+            incremented_ascii = (ord(char) + 1) % 122
+            new_word += chr(incremented_ascii)
+
+        return new_word
+        
