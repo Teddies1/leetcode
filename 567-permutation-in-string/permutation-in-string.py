fa8fbabe204c6d7bd766cdd51af4783df2a37ec2
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-07-24 22:46:57 +0800

    Time: 15 ms (85.54%) | Memory: 17.9 MB (63.34%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,40 +1,43 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        k = len(s1)
-        if s1 == s2:
-            return True
-        if len(s2) < len(s1):
+        if not s1 or not s2:
             return False
+
+        n = len(s1)
+        m = len(s2)
+        if n == 0 or m == 0:
+            return False
+        if n > m:
+            return False
+
+        target_hash = 0
+        s1_freqmap = Counter(s1)
+
+        for char in s1:
+            hash_value = ord(char)
+            target_hash += hash_value
+
+        rolling_hash = 0
+        for i in range(n-1):
+            rolling_hash += ord(s2[i])
         
-        dict1, dict2 = {}, {}
-        for i in range(len(s1)):
-            if s1[i] not in dict1:
-                dict1[s1[i]] = 1
-            else: 
-                dict1[s1[i]] += 1
-        for i in range(len(s1)):
-            if s2[i] not in dict2:
-                dict2[s2[i]] = 1
-            else:
-                dict2[s2[i]] += 1 
-                
         left = 0
-        for right in range(k, len(s2)):
-            if dict1 == dict2:
-                return True
-            
-            word = s2[left]
-            if word in dict2:
-                if dict2[word] > 1:
-                    dict2[word] -= 1
-                else:
-                    dict2.pop(word)
-             
-            word = s2[right]
-            if word not in dict2:
-                dict2[word] = 1
-            else: 
-                dict2[word] += 1 
+        for right in range(n-1, m):
+            rolling_hash += ord(s2[right])
+            if rolling_hash == target_hash:
+                if self.is_permutation(left, right, s1_freqmap, s2):
+                    return True
+            rolling_hash -= ord(s2[left])
             left += 1
-        
-        return dict1 == dict2
+
+        return False
+
+    def is_permutation(self, left, right, s1_freqmap, s2):
+        s2_freqmap = {}
+        for i in range(left, right+1):
+            if s2[i] not in s2_freqmap:
+                s2_freqmap[s2[i]] = 1
+            else:
+                s2_freqmap[s2[i]] += 1
+
+        return s1_freqmap == s2_freqmap
