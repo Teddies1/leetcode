5f5d823a0ff8f155c09ca964631c4b721311cde9
honjoo <64689788+Teddies1@users.noreply.github.com>
2025-10-06 22:12:51 +0800

    Time: 269 ms (30.44%) | Memory: 20.3 MB (58.02%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ans = 0
+        if grid is None:
+            return 0
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    ans += 1
-                    self.floodfill(grid, i, j)
+        rows = len(grid)
+        cols = len(grid[0])
         
-        return ans
-    
-    def floodfill(self, arr, row, col):
-        if row < 0 or col < 0 or row >= len(arr) or col >= len(arr[0]) or arr[row][col] == "0":
-            return 
-        
-        arr[row][col] = "0"
+        if rows == 0:
+            return 0
 
+        num_islands = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    num_islands += 1
+                    self.dfs(grid, i, j)
+            
+        return num_islands
+    
+    def dfs(self, grid, row, col):
         neighbours = [
             (0, 1),
-            (-1, 0),
-            (1, 0),
             (0, -1),
+            (1, 0),
+            (-1, 0),
         ]
-
-        for dx, dy in neighbours:
-            self.floodfill(arr, row + dx, col + dy)
+        rows = len(grid)
+        cols = len(grid[0])
         
-        
+        if 0 <= row < rows and 0 <= col < cols and grid[row][col] == "1":
+            grid[row][col] = "0"
+            for dx, dy in neighbours:
+                self.dfs(grid, row + dx, col + dy)
